cba1e98798ba33bfd007b494fe3538ca27258b47
Denlich <muchalovych@gmail.com>
2023-06-27 21:03:59 +0300

    Issue: Fixed api-client

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import authStore from "../stores/auth-store";
 
 const axiosInstance = axios.create({
   baseURL: "https://mowee.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = authStore.getState().token;
   if (token) {
     config.headers["Authorization"] = `Bearer ${token}`;
   }
